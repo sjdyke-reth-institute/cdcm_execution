97e99d2098ae9443d45cb35b29148faa96da2893
Ilias Bilionis <ebilionis@gmail.com>
2022-03-10 15:32:47 -0500

    Added _parameter.

---
+++ cdcm/_parameter.py
@@ -0,0 +1,78 @@
+"""
+Defies a Parameter class.
+
+Author:
+	Ilias Bilionis
+	Roman
+
+
+ROMAN read PEP 8 https://peps.python.org/pep-0008/
+
+
+Date:
+	3/10/2022
+"""
+
+
+__all__ = ['Parameter']
+
+
+import numpy as np
+import pint
+
+
+ureg = pint.UnitRegistry()
+
+
+class Parameter(object):
+
+	"""
+	Defines a CDCM parameter. The parameter knows its units.
+	It has a decscription that explains what it is.
+	It has a name. And it has a value.
+
+	Arguments:
+
+		value:      The value of the parameter. Must be an int, a double or a 
+				    numpy array of ints or floating point numbers.
+		units:      Must be a string or a pint object that describes an SI
+				    physical unit.
+		name:       A string. The name of the parameter. Please be expressive.
+		desciption: A desciption of the parameter. Please be expressive.
+
+	"""
+
+	def __init__(self, value, units, name, description=None):
+		# Sanity checks
+		assert isinstance(value, int) or isinstance(value, float) or \
+			(isinstance(value, np.ndarray) and value.dtype == float)
+		ureg.check(units)
+		assert isinstance(name, str)
+		assert isinstance(description, str)
+		# Assign values
+		self._value = value
+		self._units = units 
+		self._name = name
+		self._description = description
+
+	@property
+	def value(self):
+		return self._value
+
+	@property
+	def units(self):
+		return self._units
+
+	@property
+	def name(self):
+		return self._name
+	
+	@property
+	def description(self):
+		return self._description
+	
+	def __str__(self):
+		return 'Parameter(value=' + str(self.value) + \
+			', units="' + self.units + '"' + \
+			', name="' + self.name + '"' + \
+			', description="' + self.description + '")'
