ededfa38467a5582b1898f8babff29670e29164f
Ilias Bilionis <ebilionis@gmail.com>
2022-03-11 15:21:37 -0500

    Added Parameter, StateVariable, PhysicalStateVariable, HealthStateVariable.

--- cdcm/_quantity.py
+++ cdcm/_quantity.py
@@ -13,7 +13,8 @@ TODO:
 """
 
 
-__all__ = ['Quantity']
+__all__ = ['Quantity', 'Parameter', 
+		   'StateVariable', 'PhysicalStateVariable', 'HealthStateVariable']
 
 
 import numpy as np
@@ -69,10 +70,58 @@ class Quantity(object):
 	@property
 	def description(self):
 		return self._description
+
+	@property
+	def type(self):
+		return type(self).__name__
+	
 	
 	def __str__(self):
-		return 'Quantity(value=' + str(self.value) + \
+		# TODO: Fix this so that it prints the name of the clas
+		return self.type + '(value=' + str(self.value) + \
 			', units="' + self.units + '"' + \
 			', name="' + self.name + '"' + \
 			', description="' + self.description + '")'
 
+
+class Parameter(Quantity):
+
+	"""
+	A class representing a parameter of a system.
+	"""
+
+	pass 
+
+
+class StateVariable(Quantity):
+
+	"""
+	A class representing a system state variable.
+	"""
+
+	pass
+
+
+class PhysicalStateVariable(StateVariable):
+
+	"""
+	A class representing a physical system state variable.
+	"""
+
+	pass 
+
+
+class HealthStateVariable(StateVariable):
+
+	"""
+	A class representing a health state variable.
+	"""
+
+	pass
+
+
+if __name__ == '__main__':
+	p = PhysicalStateVariable(10.0, "meters", "length")
+	print(type(p).__name__)
+	print(p.type)
+	print(str(p))
