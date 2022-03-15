e381595ea293deabf2f20442eb479a0e9024ea65
Ilias Bilionis <ebilionis@gmail.com>
2022-03-15 09:45:34 -0400

    Added System from function functionality.

---
+++ tests/test_system_from_function.py
@@ -0,0 +1,34 @@
+"""Test the SystemFromFunction class.
+
+Author:
+    Ilias Bilionis
+
+Date:
+    3/15/2022
+"""
+
+
+from cdcm import *
+
+
+def transition_func(dt, x=0.1, r=1.1):
+    """A simple transition function for an isolated system.
+
+    Note that the default parameters are ignored when this
+    function is actually used by `SystemFromFunction`.
+    """
+    new_x = x + r * dt
+    new_state = {'x': new_x}
+    return new_state
+
+
+if __name__ == "__main__":
+    sys = SystemFromFunction(
+        state={"x": PhysicalStateVariable(0.1, "meters", "x")},
+        parameters={"r": Parameter(1.2, "meters / second", "rate of change")},
+        transition_func=transition_func)
+    print(sys)
+    dt = 0.1
+    for i in range(10):
+        sys.step(dt)
+        print(f"x: {sys.state['x'].value:{1}.{3}}")
