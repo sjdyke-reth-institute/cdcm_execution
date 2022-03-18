c62cbe2b4dc13d266217ed0eb5a388549b4b8e7e
Ilias Bilionis <ebilionis@gmail.com>
2022-03-18 10:37:11 -0400

    conforming to pep8

--- tests/test_nested_system_of_systems.py
+++ tests/test_nested_system_of_systems.py
@@ -1,10 +1,10 @@
 """Test a nested system of systems.
 
 Author:
-	Ilias Bilionis
+    Ilias Bilionis
 
 Date:
-	3/15/2022
+    3/15/2022
 
 """
 
@@ -18,6 +18,7 @@ def trans_func_1(dt, *, x1, r1):
     new_state = {'x1': new_x1}
     return new_state
 
+
 def trans_func_2(dt, *, x2, x1, r2, c):
     """Another simple transition function."""
     new_x2 = x2 + r2 * dt + c * x1 * dt
@@ -30,6 +31,7 @@ def trans_func_3(dt, *, x3, r3):
     new_state = {'x3': new_x3}
     return new_state
 
+
 def trans_func_4(dt, *, x4, x2, r4, c24):
     """Another simple transition function."""
     new_x4 = x4 + r4 * dt + c24 * x2 * dt
@@ -38,13 +40,13 @@ def trans_func_4(dt, *, x4, x2, r4, c24):
 
 if __name__ == "__main__":
     sys1 = SystemFromFunction(
-    	name="system_1",
+        name="system_1",
         state=PhysicalStateVariable(0.1, "meters", "x1"),
         parameters=Parameter(1.2, "meters / second", "r1"),
         transition_func=trans_func_1
     )
     sys2 = SystemFromFunction(
-    	name="system_2",
+        name="system_2",
         state=PhysicalStateVariable(0.1, "meters", "x2"),
         parameters=[
             Parameter(0.2, "meters / second", "r2"),
@@ -54,16 +56,16 @@ if __name__ == "__main__":
         transition_func=trans_func_2
     )
     sos1 = SystemOfSystems(
-        name="combined_system_1", 
+        name="combined_system_1",
         sub_systems=[sys1, sys2])
     sys3 = SystemFromFunction(
-    	name="system_3",
+        name="system_3",
         state=PhysicalStateVariable(0.1, "meters", "x3"),
         parameters=Parameter(1.2, "meters / second", "r3"),
         transition_func=trans_func_3
     )
     sys4 = SystemFromFunction(
-    	name="system_4",
+        name="system_4",
         state=PhysicalStateVariable(0.1, "meters", "x4"),
         parameters=[
             Parameter(0.2, "meters / second", "r4"),
@@ -73,7 +75,7 @@ if __name__ == "__main__":
         transition_func=trans_func_4
     )
     sos2 = SystemOfSystems(
-        name="combined_system_2", 
+        name="combined_system_2",
         sub_systems=[sys3, sys4]
     )
     sys = SystemOfSystems(
@@ -84,4 +86,5 @@ if __name__ == "__main__":
     dt = 0.1
     for i in range(10):
         sys.unsafe_step(dt)
-        print(f"x1: {sys.state['x1'].value:{1}.{3}}, x2: {sys.state['x2'].value:{1}.{3}}")
+        print(f"x1: {sys.state['x1'].value:{1}.{3}},"
+              + f"x2: {sys.state['x2'].value:{1}.{3}}")
