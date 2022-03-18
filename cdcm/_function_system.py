67f669fbbe317f85acb03fe682d427ef1563eb9f
Ilias Bilionis <ebilionis@gmail.com>
2022-03-18 10:22:46 -0400

    conforming to pep8

--- cdcm/_function_system.py
+++ cdcm/_function_system.py
@@ -29,7 +29,7 @@ class SystemFromFunction(System):
                        as follows:
                        ```
                        def transition_func(dt, *, state1, ...
-                                           parent_var1, ..., 
+                                           parent_var1, ...,
                                            parameter_1, ...):
                            # Do the calculations
                            # Make a return a new_state dictionary for the form:
@@ -38,39 +38,34 @@ class SystemFromFunction(System):
                                         ...}
                            return new_state
                        ```
-                       Notice the "*" in the function definition. It is essential.
-                       Do not skip it!
+                       Notice the "*" in the function definition. It is
+                       essential. Do not skip it!
     description     -- A description for the object.
     """
 
-    def __init__(
-            self, 
-            name="system_from_function", 
-            state={}, 
-            parameters={}, 
-            parents={}, 
-            transition_func=None, 
-            test_func=False,
-            description="",
-            default_dt=1e-3
-        ):
-        super().__init__(
-                    name=name, 
-                    state=state, 
-                    parameters=parameters, 
-                    parents=parents, 
-                    description=description
-                )
+    def __init__(self,
+                 name="system_from_function",
+                 state={},
+                 parameters={},
+                 parents={},
+                 transition_func=None,
+                 test_func=False,
+                 description="",
+                 default_dt=1e-3):
+        super().__init__(name=name,
+                         state=state,
+                         parameters=parameters,
+                         parents=parents,
+                         description=description)
         assert callable(transition_func)
         self._transition_func = transition_func
-
 
     def _calculate_next_state(self, dt):
         inputs = {}
         for k, var in self.state.items():
             inputs[k] = var.value
         for k, var in self.parameters.items():
-            inputs[k] = var.value 
+            inputs[k] = var.value
         for k, var in self.parents.items():
             inputs[k] = var.state[k].value
         new_state = self._transition_func(dt, **inputs)
