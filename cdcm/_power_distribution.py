1cf170080dfdb514cccc9715e0d8f5d7c512b70f
Ilias Bilionis <ebilionis@gmail.com>
2022-03-21 17:05:07 -0400

    Still thinking about the power distribution.

---
+++ cdcm/_power_distribution.py
@@ -0,0 +1,27 @@
+"""A clas representing a power distribution system.
+
+Author:
+    Ilias Bilionis
+
+Date:
+    3/21/2022
+
+"""
+
+
+from . import System
+
+
+class PowerDistributionSystem(System):
+    """A power distribution system.
+
+    Keyword Arguments:
+    name
+    description
+    """
+
+    def __init__(
+        name="power_disribution",
+        description=""
+    ):
+        pass
