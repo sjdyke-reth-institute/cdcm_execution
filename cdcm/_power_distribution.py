3e41c54d3306c28db572d35fed86312e8d9965ae
Ilias Bilionis <ebilionis@gmail.com>
2022-03-26 19:08:52 -0400

    Working on power distribution.

--- cdcm/_power_distribution.py
+++ cdcm/_power_distribution.py
@@ -9,7 +9,7 @@ Date:
 """
 
 
-from . import System
+from . import System, PowerGenerator, PowerConsumer
 
 
 class PowerDistributionSystem(System):
@@ -37,7 +37,83 @@ class PowerDistributionSystem(System):
     """
 
     def __init__(
+        self,
         name="power_disribution",
+        state={},
+        parameters={},
+        parents={},
+        sub_systems={},
         description=""
     ):
-        pass
+        super().__init__(
+            name=name,
+            description=description,
+            state=state,
+            parameters=parameters,
+            parents=parents,
+            sub_systems=sub_systems
+        )
+        self._generator_names = []
+        self._consumer_names = []
+
+    @property
+    def generator_names(self):
+        """Get the names of the parents that are generators."""
+        return self._generator_names
+
+    @property
+    def consumer_names(self):
+        """Get the names of the parents that are consumers."""
+        return self._consumer_names
+
+    def add_generator(self, system, generator_name=None):
+        """Adds a generator to the distribution network.
+
+        Arguments:
+        system     -- The `PowerGenerator` system to connect.
+
+        Keyword Arguments
+        generator_name -- The name used to refer to this power
+                          generator. If not specified, then
+                          we use "power_input_<count>" where <count>
+                          starts from zero and counts the number of
+                          generators added so far.
+        """
+        assert isinstance(system, PowerGenerator)
+        if generator_name is None:
+            generator_name = f"power_input_{len(self.generator_names)}"
+        self.add_parent(generator_name, system, "power_output")
+
+    @property
+    def power_in(self):
+        """Get the total power generated during this timestep."""
+        s = 0.0
+        for g_name in self.generator_names:
+            s += self.get_parent_state(g_name).value
+        return s
+
+    def add_consumer(self, system, consumer_name=None):
+        """Adds a consumer to the distribution network.
+
+        Arguments:
+        system     -- The `PowerConsumer` system to connect.
+
+        Keyword Arguments
+        consumer_name -- The name used to refer to this power
+                          consumer. If not specified, then
+                          we use "power_output_<count>" where <count>
+                          starts from zero and counts the number of
+                          generators added so far.
+        """
+        assert isinstance(system, PowerConsumer)
+        if consumer_name is None:
+            consumer_name = f"power_output_{len(self.consumer_names)}"
+        self.add_parent(consumer_name, system, "required_power")
+
+    @property
+    def required_power(self):
+        """Get the total power required during this timestep."""
+        s = 0.0
+        for c_name in self.consumer_names:
+            s += self.get_parent_state(c_name).value
+        return s
