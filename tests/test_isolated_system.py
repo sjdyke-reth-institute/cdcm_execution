062cb2789fdc60f67e42cf3880958189a30211e7
Ilias Bilionis <ebilionis@gmail.com>
2022-03-29 19:15:11 -0400

    Minor changes.

--- tests/test_isolated_system.py
+++ tests/test_isolated_system.py
@@ -10,51 +10,34 @@ Date:
 """
 
 
-from cdcm import (System,
-                  Parameter,
-                  PhysicalStateVariable,
-                  HealthStateVariable)
+from cdcm import *
 
 
-class IsolatedSystem(System):
+x = PhysicalStateVariable(
+    value=0.1,
+    units="meters",
+    name="x",
+    track=True,
+    description="The x variable."
+)
 
-    def __init__(self):
-        name = "Isolated System"
-        state = [
-            PhysicalStateVariable(value=0.1,
-                                  units="meters",
-                                  name="x",
-                                  track=True,
-                                  description="The x variable."),
-            HealthStateVariable(value=0,
-                                units="",
-                                name="h",
-                                track=True,
-                                description="The h variable.")
-        ]
-        parameters = Parameter(value=1.2,
-                               units="meters / second",
-                               name="rate_of_change",
-                               description="The rate of change.")
-        super().__init__(
-            name=name,
-            state=state,
-            parameters=parameters,
-            description="A simple isolated system."
-        )
-
-    def _calculate_my_next_state(self, dt):
-        x = self.state['x'].value
-        r = self.parameters['rate_of_change'].value
-        self._next_state['x'].value = x + r * dt
+r = Parameter(
+    value=1.2,
+    units="meters / second",
+    name="r",
+    description="The rate of change."
+)
 
 
-if __name__ == "__main__":
-    system = IsolatedSystem()
-    print(system.state)
-    print(str(system))
-    # Run the system a bit into the future manually.
-    dt = 0.1
-    for i in range(10):
-        system.unsafe_step(dt)
-        print(f"x: {system.state['x']}")
+@make_system
+def sys(dt, *, x=x, r=r):
+    return x + r * dt
+
+
+print(sys)
+
+
+dt = 0.1
+for i in range(10):
+    sys.unsafe_step(dt)
+    print(f"x: {sys.state['x']}")
