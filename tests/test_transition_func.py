7ee1739371b31510ca0f3491c2e715f5c015857a
Ilias Bilionis <ebilionis@gmail.com>
2022-04-13 00:37:33 -0400

    Transition seems okay.

--- tests/test_transition_func.py
+++ tests/test_transition_func.py
@@ -14,6 +14,9 @@ from cdcm import *
 import yaml
 
 
+print("TEST 1")
+print("*" * 80)
+
 # We will make a simple system with one state first
 x = State(
     name="x",
@@ -53,3 +56,45 @@ f()
 x._transition()
 print("State after:")
 print(yaml.dump(x.to_yaml(), sort_keys=False))
+
+# Now let's test a transition function that updates two states at the
+# same time
+print("TEST 2")
+print("*" * 80)
+x1 = State(
+    name="x1",
+    value=1.0,
+    units="m"
+)
+x2 = State(
+    name="x2",
+    value=2.0,
+    units="m"
+)
+g = TransitionFunction(
+    name="g",
+    parents=[x1, x2, r, dt],
+    children=[x1, x2],
+    transition_func=lambda *, x1, x2, r, time_step: (
+        x1 + r * time_step,
+        x2 + x1 * time_step
+    )
+)
+# Print the entire computational graph
+print(yaml.dump(x1.to_yaml(), sort_keys=False))
+print(yaml.dump(x2.to_yaml(), sort_keys=False))
+print(yaml.dump(r.to_yaml(), sort_keys=False))
+print(yaml.dump(dt.to_yaml(), sort_keys=False))
+print(yaml.dump(g.to_yaml(), sort_keys=False))
+
+# Test the transition
+print("State before:")
+print(yaml.dump(x1.to_yaml(), sort_keys=False))
+print(yaml.dump(x2.to_yaml(), sort_keys=False))
+print("Evaluating the next step.")
+g()
+x1._transition()
+x2._transition()
+print("State after:")
+print(yaml.dump(x1.to_yaml(), sort_keys=False))
+print(yaml.dump(x2.to_yaml(), sort_keys=False))
