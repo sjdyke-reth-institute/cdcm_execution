238ade6c09fd48b6d86170e4a843ccb14668d526
Ting-Chun Kuo <kuo63@purdue.edu>
2022-05-24 14:13:14 -0400

    add noise

--- examples/multizone_rc_run.py
+++ examples/multizone_rc_run.py
@@ -36,43 +36,57 @@ Q_int = Variable(
     description="Sum of internal heat gain"
 )
 
-T_cor = Variable(
-    name="T_cor",
+T_cor1 = Variable(
+    name="T_cor1",
     units="degC",
     value=23,
     description="Measured corridor temperature for zone 1."
+)
+
+T_cor2 = Variable(
+    name="T_cor2",
+    units="degC",
+    value=23,
+    description="Measured corridor temperature for zone 2."
 )
 
 clock = make_clock(1800)
 
 rc_sys = RCBuildingSystem(clock.dt,
                           weather_sys,
-                          T_cor,
+                          T_cor1,
                           Q_int,
                           name="rc_sys_1")
 
 rc_sys2 = RCBuildingSystem(clock.dt,
                            weather_sys,
-                           rc_sys.T_room,
+                           T_cor2,
                            Q_int,
                            name="rc_sys_2")
 
 T_room_sensor_sigma = Parameter(
-    name="T_room_sensor_sigma",
+    name="T_room1_sensor_sigma",
     units="degC",
     value=0.1
 )
 
 
-@make_function(T_cor)
-def g_T_cor_sensor(T_neighbor=rc_sys2.T_room, sigma=T_room_sensor_sigma):
+@make_function(T_cor1)
+def g_T_cor1_sensor(T_neighbor=rc_sys2.T_room, sigma=T_room_sensor_sigma):
+    """Sample the T_out sensor."""
+    return T_neighbor + sigma * np.random.randn()
+
+
+@make_function(T_cor2)
+def g_T_cor2_sensor(T_neighbor=rc_sys.T_room, sigma=T_room_sensor_sigma):
     """Sample the T_out sensor."""
     return T_neighbor + sigma * np.random.randn()
 
 
 sys = System(
     name="everything",
-    nodes=[clock, weather_sys, rc_sys, rc_sys2, T_cor, g_T_cor_sensor]
+    nodes=[clock, weather_sys, rc_sys, rc_sys2, T_cor1, T_cor2,
+           g_T_cor1_sensor, g_T_cor2_sensor]
 )
 
 print(sys)
@@ -82,6 +96,7 @@ print(sys.rc_sys_1)
 for i in range(100):
     sys.forward()
     print(f"T_room1 = {rc_sys.T_room.value:1.2f}")
-    print(f"T_cor1 = {T_cor.value:1.2f}")
+    print(f"T_cor1(Measured for room 2) = {T_cor1.value:1.2f}")
     print(f"T_room2 = {rc_sys2.T_room.value:1.2f}")
+    print(f"T_cor2(Measured for room 1) = {T_cor2.value:1.2f}")
     sys.transition()
