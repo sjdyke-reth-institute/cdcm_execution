8da3179c2e3959137544128be00695170b4fb7bb
Ilias Bilionis <ebilionis@gmail.com>
2022-06-21 15:14:30 -0400

    Update habitat.py

--- smart_hab/habitat.py
+++ smart_hab/habitat.py
@@ -12,11 +12,10 @@ path_data_files = './data_files/'
 
 with System(name="everything",
     description="Everything that goes in the simulation") as everything:
-    pass
     # Make a clock
     clock = make_clock(3600.0)
     dome_specs = make_dome_specs()
-    moon = make_moon(path_data_files, clock, dome_specs)
-
+    # TODO: comment on decomposition
+    moon_env = make_moon(path_data_files, clock, dome_specs)
 
 print(everything)
