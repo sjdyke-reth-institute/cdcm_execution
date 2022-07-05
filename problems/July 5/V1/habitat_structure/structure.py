dec5891d99dba6bb402fb29793a709e02010c826
Amir <88452345+abehjat-purdue@users.noreply.github.com>
2022-07-05 14:21:23 -0400

    A problem faced using replace
    Here are the two folders that make the different outputs. “compare_and_check_all_files.py” shows the comparison, but I added the “1 - Diff Checker.pdf” made by the diffchecker website. It is more than anything odd for me. But I will see.
    I am also sure it is due to the “replace” part. I check if there is any alternative to replace to do it. The last time I fixed it, the fixture was based on reordering, but I will search for a better solution this time.

---
+++ problems/July 5/V1/habitat_structure/structure.py
@@ -0,0 +1,63 @@
+"""
+Defines the struct interface/type/system/concept.
+
+A `structEnvironment` is `System` that exposes the following interface:
+
+
+variable :: TypeOfVariable
+
+(variable) = optional variable
+
+                                               _________________
+moon             :: Moon                   => |                 | -> strucure_health         :: State[array]
+(int_env)        :: InteriorEnv            => |    StructEnv    |
+agents           :: Agents                 => |                 |
+design           :: DomeSpec               => |_________________| -> strucure_temp_innerside :: State
+
+"""
+
+
+
+__all__ = ["make_structure"]
+
+
+from cdcm import *
+from . import make_struct_health_env_0
+from . import make_struct_temp_env_0
+
+from dome_design import *
+
+
+def make_structure(moon,
+                   dome_specs,
+                   interior_environment_int_env_temp=None,
+                   agent_repair_struct=None,
+                   make_struct_health_env=make_struct_health_env_0,
+                   make_struct_temp_env=make_struct_temp_env_0,
+                   ):
+    """
+    Make a struct system.
+
+    Arguments
+    moon
+    dome_specs,
+    interior_environment_int_env_temp
+    agent_dome_repair
+
+    """
+    with System(name="struct", description="The struct system") as struct:
+        # if interior_environment_int_env_temp is None:
+        #     interior_environment_int_env_temp = Variable(name="int_env_temp", value=280.0, units="K", description="Interior Environment Temperature")
+        # if agent_repair_struct is None:
+        #     agent_repair_struct = Variable(name="agent_repair_struct", value=[0.0, 0.0, 0.0, 0.0, 0.0], units="", description="Agent's given health improvment for each dome section")
+        struct_health = make_struct_health_env(moon,
+                                               agent_repair_struct)
+
+        struct_temp = make_struct_temp_env(struct_health,
+                                           dome_specs,
+                                           moon,
+                                           interior_environment_int_env_temp
+                                           )
+
+    return struct
+
---
+++ problems/July 5/V2/habitat_energy/__init__.py
@@ -0,0 +1,15 @@
+"""
+All the energy models.
+
+TODO: Write me.
+
+"""
+
+# from .utils import *
+from .energy_performance import *
+from .energy_generate import *
+from .energy_store import *
+
+from .energy import *
+
+# etc.
