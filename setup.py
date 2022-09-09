cae25e572dc702cf5d67527574826e1b78f3046c
murakrishn <murakrishn@users.noreply.github.com>
2022-09-09 16:08:52 -0400

    added `requirements.txt`

--- setup.py
+++ setup.py
@@ -7,15 +7,20 @@ with open('README.md') as f:
 with open('LICENSE') as f:
     license = f.read()
 
+with open('requirements.txt') as f:
+    requirements = [line.strip() for line in f]
+
 setup(
     name='cdcm',
     version='0.0.1',
-    description='Control-oriented Dynamic Computational Platform (CDCM)',
+    description='Control-oriented Dynamic Computational Modeling Platform (CDCM)',
     long_description=readme,
     author='',
     author_email='',
     url='',
     license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
+    packages=find_packages(exclude=('tests', 'docs')),
+    python_requires=">=3.6",
+    install_requires=requirements,
 )
 
