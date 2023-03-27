7a379f1172b93a47c09c6ff75258ed4ea1b0e77b
murakrishn <murakrishn@users.noreply.github.com>
2023-03-27 14:33:51 -0400

    ability to _maybe_ construct different types of System objects

--- cdcm_hab_mcvt/abstractions/common.py
+++ cdcm_hab_mcvt/abstractions/common.py
@@ -19,11 +19,11 @@ from cdcm import System
 from typing import Union
 
 
-def maybe_make_system(name_or_system: Union[str, System], **kwargs):
+def maybe_make_system(name_or_system: Union[str, System], _Constructor = System, **kwargs):
     """Returns either a new system with a given name or the system that is provided."""
     if isinstance(name_or_system, str):
         # I am making a new system
-        sys = System(name=name_or_system, **kwargs)
+        sys = _Constructor(name=name_or_system, **kwargs)
     elif isinstance(name_or_system, System):
         # I am just adding variables to an existing system
         sys = name_or_system
