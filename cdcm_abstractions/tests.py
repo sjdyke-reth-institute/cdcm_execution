19b0590a49de5d89a094d15cf3f3a54b040a21b1
murakrishn <murakrishn@users.noreply.github.com>
2023-04-01 14:03:32 -0400

    working on test constructor mechanism.

--- cdcm_abstractions/tests.py
+++ cdcm_abstractions/tests.py
@@ -11,9 +11,25 @@ Date:
 
 
 from cdcm import *
+from cdcm_abstractions import *
+
+from typing import Set, Union, NewType
+
+
+SetofStatusVariables = NewType("SetofStatusVariables", Set["HealthStatus"])
 
 
 class Test(Variable):
     """Result of a diagnostic test"""
     def __init__(self, name: str, **kwargs):
         super().__init__(name=name, **kwargs)
+
+
+
+def make_test(
+        name: Union[str, Test], 
+        status_vars: SetofStatusVariables, 
+        **kwargs) -> Test:
+    """A function that creates a test variable"""
+
+    pass
