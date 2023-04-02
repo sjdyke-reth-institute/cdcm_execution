eef56b315793086f2f0176ca9450f65cd778f1e8
murakrishn <murakrishn@users.noreply.github.com>
2023-04-01 21:13:51 -0400

    decorator patterns for instantiating and setting values for functionality and test variables

--- cdcm_abstractions/tests.py
+++ cdcm_abstractions/tests.py
@@ -11,12 +11,12 @@ Date:
 
 
 from cdcm import *
-from cdcm_abstractions import *
+from .health_status import *
 
-from typing import Set, Union, NewType
+from typing import Set, Union, NewType, Callable
 
 
-SetofStatusVariables = NewType("SetofStatusVariables", Set["HealthStatus"])
+SetofStatusVariables = NewType("SetofStatusVariables", Set['HealthStatus'])
 
 
 class Test(Variable):
@@ -26,10 +26,26 @@ class Test(Variable):
 
 
 
-def make_test(
-        name: Union[str, Test], 
-        status_vars: SetofStatusVariables, 
-        **kwargs) -> Test:
-    """A function that creates a test variable"""
+def make_test(test_name: str="test", *args, **kwargs) -> Test:
+    """A function that creates a test variable with a functional dependency"""
 
-    pass
+    def make_test_inner(test_func: Callable):
+
+        signature = get_default_args(test_func)
+        parents = signature.values()
+
+        test_var = Test(
+            name=test_name,
+            value=0.,
+            description="Results of test"
+        )
+        fn_test_var = Function(
+            name=test_func.__name__,
+            children=test_var,
+            parents=parents,
+            func=test_func,
+            description=f"Definition of procedure that sets value of {test_var.absname}"
+        )
+        return test_var
+
+    return make_test_inner
