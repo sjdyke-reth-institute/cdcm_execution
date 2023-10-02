43a034287fe6447eed99c311f79c2fec8132f4b6
murakrishn <murakrishn@users.noreply.github.com>
2023-10-02 12:30:21 -0400

    test demonstrating variable transformations using abstractions

---
+++ tests/cdcm_abstractions/test_variable_behaviors.py
@@ -0,0 +1,37 @@
+"""Test transformations to Variable nodes
+
+Author:
+    R Murali Krishnan
+    
+Date:
+    10.02.2023
+    
+"""
+
+
+from cdcm import System, Variable
+from cdcm_abstractions import scale, transform 
+from cdcm_utils import make_pyvis_graph
+
+
+with System(name="mysys") as mysys:
+
+    x = Variable(name="x", value=1.0)
+
+    # scale `x` (default name is `<var_name>_scaled`)
+    x_scaled = scale(x, 10.0)
+
+    # transform `x` (default name is `<var_name>_transformed`)
+    x_transformed = transform(x, func=lambda _x: 1.0 - _x / 5.0)
+
+
+print(mysys)
+mysys.forward()
+
+
+print(f"x.value: {x.value}, " + \
+      f"x_scaled.value: {x_scaled.value}, " + \
+      f"x_transformed.value: {x_transformed.value}")
+
+g = make_pyvis_graph(mysys)
+g.show("test_variable_behaviors.html")
